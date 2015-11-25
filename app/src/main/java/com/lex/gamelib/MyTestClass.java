96fef0fd34e5da7a2505a136039c4671759de96d
Oleksiy Martynov <3martynov@gmail.com>
2015-11-24 21:19:57 -0500

    -initial

---
+++ app/src/main/java/com/lex/gamelib/MyTestClass.java
@@ -0,0 +1,13 @@
+package com.lex.gamelib;
+
+/**
+ * Created by Oleksiy on 11/24/2015.
+ */
+public class MyTestClass {
+    public static String getHello(){
+        return "Hello from lib!";
+    }
+    public static String getGoodbye() {
+        return "Goodbye from lib!";
+    }
+}
